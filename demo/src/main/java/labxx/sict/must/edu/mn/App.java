65ed9735bde2c419be80fce99a99c50f8c5667e2
Assaaku <110076008+Assaaku@users.noreply.github.com>
2025-05-09 13:51:51 +0800

    Maven project file and other such things added

---
+++ demo/src/main/java/labxx/sict/must/edu/mn/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+//asdasd
+//asdd
+
+
+public class App 
+{
+
+
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
